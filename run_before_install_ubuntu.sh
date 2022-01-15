305e0a26694217474f0baf4a051388f1a83001a7
plusplushq <34548660+plusplushq@users.noreply.github.com>
2022-01-15 11:30:48 -0800

    Update run_before_install_ubuntu.sh

--- run_before_install_ubuntu.sh
+++ run_before_install_ubuntu.sh
@@ -1,9 +1,11 @@
 sudo apt update
 sudo apt upgrade
+sudo apt install zsh
+chsh -s zsh
 
 # Install Homebrew
-sudo apt-get install build-essential procps curl file git
-mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-eval "$(homebrew/bin/brew shellenv)"
-brew update --force --quiet
-chmod -R go-w "$(brew --prefix)/share/zsh"
+# sudo apt-get install build-essential procps curl file git
+# mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+# eval "$(homebrew/bin/brew shellenv)"
+# brew update --force --quiet
+# chmod -R go-w "$(brew --prefix)/share/zsh"
