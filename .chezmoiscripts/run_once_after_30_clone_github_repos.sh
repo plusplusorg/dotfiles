bd41d488a9d7ac4b7541fa7fcab8c517a50a3049
plusplushq <34548660+plusplushq@users.noreply.github.com>
2022-04-19 23:52:11 -0700

    Update run_once_after_30_clone_github_repos.sh

--- .chezmoiscripts/run_once_after_30_clone_github_repos.sh
+++ .chezmoiscripts/run_once_after_30_clone_github_repos.sh
@@ -1,8 +1,9 @@
-gh repo clone plusplushq/chakra-ui
-gh repo clone plusplushq/next.js
-gh repo clone plusplushq/redux-toolkit
-gh repo clone plusplushq/react-hook-form
-gh repo clone plusplushq/digital-garden
-gh repo clone plusplushq/tensorui
-gh repo clone plusplushq/zettelkasten
-gh repo clone plusplushq/nx
+
+# cd ~ && gh repo clone plusplushq/chakra-ui
+# cd ~ && gh repo clone plusplushq/next.js
+# cd ~ && gh repo clone plusplushq/redux-toolkit
+# cd ~ && gh repo clone plusplushq/react-hook-form
+# cd ~ && gh repo clone plusplushq/digital-garden
+# cd ~ && gh repo clone plusplushq/tensorui
+# cd ~ && gh repo clone plusplushq/zettelkasten
+# cd ~ && gh repo clone plusplushq/nx
